36153e2553522b7c1e37a6faa43bf8901e166166
Grevinna <olesya.sht@gmail.com>
2016-11-24 01:05:25 +0300

    правка кода на единообразие

--- js/script.js
+++ js/script.js
@@ -20,20 +20,50 @@ window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
     if (popup.classList.contains("form-show")) {
       popup.classList.remove("form-show");
-			popup.classList.remove("form-error");
+      popup.classList.remove("form-error");
     }
   }
 });
 
 formBtn.addEventListener("click", function(event) {
-	if (!arrivalDate.value || !dateOfDeparture.value || !adults.value || !children.value) {
-		event.preventDefault();
-		popup.classList.remove("form-error");
+  event.preventDefault();
+  var valid = true;
+
+  if (!arrivalDate.value) {
+    valid = false;
+    addErrorDecoration(arrivalDate);
+  }
+  if (!dateOfDeparture.value) {
+    valid = false;
+    addErrorDecoration(dateOfDeparture);
+  }
+  if (!adults.value || typeof adults.value != 'number') {
+    valid = false;
+    addErrorDecoration(adults);
+  }
+  if (!children.value || typeof children.value != 'number') {
+    valid = false;
+    addErrorDecoration(children);
+  }
+
+  if (!valid) {
     popup.offsetWidth = popup.offsetWidth;
-		console.log("Нужно ввести значение");
-		popup.classList.add("form-error");
-	}
-  else {
-    form.submit();
+    if (!arrivalDate.value || !dateOfDeparture.value || !adults.value || !children.value) {
+      event.preventDefault();
+      popup.classList.remove("form-error");
+      popup.offsetWidth = popup.offsetWidth;
+      console.log("Нужно ввести значение");
+      popup.classList.add("form-error");
+    } else {
+      form.submit();
+    }
+
+    function addErrorDecoration(item) {
+      item.classList.add('input-error');
+      item.addEventListener('click', function() {
+        item.classList.remove('input-error');
+      });
+    }
+
   }
 });
